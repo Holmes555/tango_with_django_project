5107e4f6c7d7b860bb3ae443616a3c11985e4dc1
Ivan <ivan.bazhenov333@gmail.com>
2017-11-19 21:53:38 +0300

    add admin cool view

--- rango/admin.py
+++ rango/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from rango.models import Category, Page
 
+
+class PageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['category']}),
+        ('Info', {'fields': ['title', 'url']}),
+        ('Popularity', {'fields': ['views']})
+    ]
+    list_display = ['title', 'category', 'url']
+
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
