d7cadcf7b74ea396ba5ca316eadf5802bbe383b9
Ivan <ivan.bazhenov333@gmail.com>
2017-11-22 15:00:12 +0300

    PEP 8 (2 line before def)

--- tango_with_django_project/views.py
+++ tango_with_django_project/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 
+
 def main(request):
     return HttpResponse('Main <br/> <a href="rango">Rango</a>'
                         '<br/> <a href="admin">Admin</a>')
