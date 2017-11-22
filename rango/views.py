42672a5c929bea4705cd19fde2396e5ca74d9877
Ivan <ivan.bazhenov333@gmail.com>
2017-11-22 14:58:47 +0300

    make categories_list to show it on the page

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,19 @@
 import django
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+from rango.models import Category
 
 
 def index(request):
-    context_dict = {'boldmessage': 'Hello from Ivan'}
+    # Query a list of categories from my database.
+    # Sort them by likes and show 5 or less
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {
+        'boldmessage': 'Hello from Ivan',
+        'categories': category_list
+    }
 
     return render(request, 'rango/index.html', context=context_dict)
 
