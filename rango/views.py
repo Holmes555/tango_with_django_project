6bdb4843f9b0670469c7bc035a60ed9e4162eb42
Ivan <ivan.bazhenov333@gmail.com>
2017-12-01 09:32:24 +0300

    Work with cookies, which have 2 values: visits and last_visit_time. Make them stored in the server. Add 2 helper functions. Show number of visits in the About page.

--- rango/views.py
+++ rango/views.py
@@ -4,24 +4,61 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from datetime import datetime
 
 from .models import Category, Page
 from .forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 
 def index(request):
+    request.session.set_test_cookie()
+
+    context_dict = {}
+
     # Query a list of categories and pages from my database.
     # Sort them by likes and show 5 or less
 
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {
-        'boldmessage': 'Hello from Ivan',
-        'categories': category_list,
-        'pages': page_list
-    }
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+
+    context_dict['boldmessage'] = 'Hello from Ivan'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+
+
+# Not a view. Just a helper function
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie, '%Y-%m-%d %H:%M:%S.%f')
+
+    print((datetime.now() - last_visit_time))
+    print((datetime.now() - last_visit_time).seconds)
+    if(datetime.now() - last_visit_time).seconds > 10:
+        visits += 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+
+# Not a view. Just a helper function
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+
+    if not val:
+        val = default_val
+
+    return val
 
 
 @login_required
@@ -148,7 +185,15 @@ def user_logout(request):
 
 
 def about(request):
-    context_dict = {'project': (str('Django ') + str(django.__version__))}
+
+    if request.session.test_cookie_worked():
+        print('Test Cookie Worked!')
+        request.session.delete_test_cookie()
+
+    context_dict = {
+        'project': (str('Django ') + str(django.__version__)),
+        'visits': request.session['visits']
+    }
 
     print(request.method)
     print(request.user)
